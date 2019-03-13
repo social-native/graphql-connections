52631375abc58abd4aa2e53918bf5c6052ce6d1d
Ethan Hathaway <erhathaway@gmail.com>
2019-03-13 16:14:19 -0700

    feat: add integration test for node transformer

--- __tests__/types.ts
+++ __tests__/types.ts
@@ -10,27 +10,4 @@ export interface IUserNode extends INode {
     bio: string;
 }
 
-// export type IUserCursorArgs = ICursorArgs;
-// export type IUserFilterArgs = FilterArgs<
-//     'id' | 'username' | 'firstname' | 'age' | 'haircolor' | 'lastname' | 'bio'
-// >;
-
-// export type FilterFields =
-//     | 'id'
-//     | 'username'
-//     | 'firstname'
-//     | 'age'
-//     | 'haircolor'
-//     | 'lastname'
-//     | 'bio';
-// export type OrderByFields =
-//     | 'id'
-//     | 'username'
-//     | 'firstname'
-//     | 'age'
-//     | 'haircolor'
-//     | 'lastname'
-//     | 'bio';
-
-// export type IUserInputs = IInputArgs<FilterFields, OrderByFields>;
 export type KnexQueryResult = Array<{[attributeName: string]: any}>;

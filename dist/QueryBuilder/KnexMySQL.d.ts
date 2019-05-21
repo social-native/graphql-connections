b7066cb5c6daeb2a27d65b5758cc6e063f10ea90
dearsaturn <seeingsaturn@gmail.com>
2019-05-21 15:17:58 -0700

    build

--- dist/QueryBuilder/KnexMySQL.d.ts
+++ dist/QueryBuilder/KnexMySQL.d.ts
@@ -1,15 +1,15 @@
 import KnexBaseQueryBuilder from './Knex';
 import Knex from 'knex';
-import { QueryContext } from 'index';
-import { IInAttributeMap, QueryBuilderOptions } from 'types';
+import { QueryContext } from '../index';
+import { IInAttributeMap, QueryBuilderOptions } from '../types';
 export default class KnexMySQLFullTextQueryBuilder extends KnexBaseQueryBuilder {
     private searchColumns;
     private searchModifier?;
     private hasSearchOptions;
     constructor(queryContext: QueryContext, attributeMap: IInAttributeMap, options?: QueryBuilderOptions);
     createQuery(queryBuilder: Knex.QueryBuilder): Knex.QueryBuilder;
-    applyRelevanceSelect(queryBuilder: Knex.QueryBuilder): Knex.QueryBuilder;
-    protected applySearch(queryBuilder: Knex.QueryBuilder): Knex.QueryBuilder | undefined;
+    applyRelevanceSelect(queryBuilder: Knex.QueryBuilder): void;
+    protected applySearch(queryBuilder: Knex.QueryBuilder): void;
     private createFullTextMatchClause;
     private isKnexMySQLBuilderOptions;
 }

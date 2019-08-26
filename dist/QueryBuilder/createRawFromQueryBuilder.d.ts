81e7bd169f724699c8c6faac267c30e930118d52
Ethan Hathaway <erhathaway@users.noreply.github.com>
2019-08-26 15:54:28 -0700

    Feat: add npm publishing (#26)
    * Feat: add npm publishing
    
    * Feat: add npm publishing
    
    * Feat: add npm publishing
    
    * Feat: add npm publishing
    
    * Feat: add npm publishing
    
    * Feat: remove dist

--- dist/QueryBuilder/createRawFromQueryBuilder.d.ts
+++
@@ -1,6 +0,0 @@
-import { QueryBuilder } from 'knex';
-/**
- * Knex does not provide a createRawFromQueryBuilder, so this fills in what Knex does in:
- * https://github.com/tgriesser/knex/blob/887fb5392910ab00f491601ad83383d04b167173/src/util/make-knex.js#L29
- */
-export default function createRawFromQueryBuilder(builder: QueryBuilder, rawSqlQuery: string, bindings?: any): import("knex").Raw;
--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { default as ConnectionManager } from './ConnectionManager';
-export { default as QueryContext } from './QueryContext';
-export { default as QueryResult } from './QueryResult';
-export { default as CursorEncoder } from './CursorEncoder';
-export { Knex, KnexMySQL } from './QueryBuilder';
-export * from './types';
-export { typeDefs, resolvers, gqlTypes } from './graphqlSchema';
